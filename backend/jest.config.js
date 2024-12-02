287c62b2ba30e9c44b2292928591c663e93da6e1
Roger <roger.runch@gmail.com>
2024-12-02 15:40:21 +0100

    Fix CI

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -4,5 +4,14 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/domain/connection/__tests__/setup.ts'],
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
-  }
+  },
+  fakeTimers: {
+    enableGlobally: true
+  },
+  testMatch: ['**/__tests__/**/*.spec.ts'],
+  testPathIgnorePatterns: [
+    '/node_modules/',
+    '.*\\.mock\\.ts$',
+    'setup\\.ts$'
+  ]
 } 
--- backend/src/domain/connection/__tests__/setup.ts
+++ backend/src/domain/connection/__tests__/setup.ts
@@ -6,4 +6,6 @@ beforeEach(() => {
 
 afterEach(() => {
   jest.resetAllMocks()
-}) 
+})
+
+export {} 
