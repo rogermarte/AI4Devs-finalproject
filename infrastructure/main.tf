fb428082143d1af2c457eb912703c595d2f4406e
Roger <roger.runch@gmail.com>
2024-12-02 20:35:17 +0100

    Listen 4000

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -344,6 +344,13 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    protocol    = "tcp"
+    from_port   = 4000
+    to_port     = 4000
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   egress {
     protocol    = "-1"
     from_port   = 0
